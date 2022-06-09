88c91cc51b0a3ffa24acd185d75c65b630d9a4a9
Elogen <github.com/elogen0>
2022-06-09 13:29:37 +0900

    DeadLock

--- ServerCore/Lock.cpp
+++ ServerCore/Lock.cpp
@@ -1,8 +1,14 @@
 #include "pch.h"
 #include "Lock.h"
+#include "CoreTLS.h"
+#include "DeadLockProfiler.h"
 
-void Lock::WriteLock()
+void Lock::WriteLock(const char* name)
 {
+#if _DEBUG
+    GDeadLockProfiler->PushLock(name);
+#endif
+
     //동일한 쓰레드가 소유하고 있다면 무조건 성공
     const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
     if (LThreadId == lockThreadId)
@@ -33,8 +39,11 @@ void Lock::WriteLock()
     }
 }
 
-void Lock::WriteUnlock()
+void Lock::WriteUnlock(const char* name)
 {
+#if _DEBUG
+    GDeadLockProfiler->PopLock(name);
+#endif
     //ReadLock 다 풀기 전에는 WriteUnlock 불가능
     if ((_lockFlag.load() & READ_COUNT_MASK) != 0)
         CRASH("INVALID_UNLOCK_ORDER");
@@ -44,8 +53,12 @@ void Lock::WriteUnlock()
         _lockFlag.store(EMPTY_FLAG);
 }
 
-void Lock::ReadLock()
+void Lock::ReadLock(const char* name)
 {
+#if _DEBUG
+    GDeadLockProfiler->PushLock(name);
+#endif
+
     // 동일한 쓰레드가 소유하고 있다면 무조건 성공
     const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
     if (LThreadId == lockThreadId)
@@ -72,8 +85,12 @@ void Lock::ReadLock()
     }
 }
 
-void Lock::ReadUnlock()
+void Lock::ReadUnlock(const char* name)
 {
+#if _DEBUG
+    GDeadLockProfiler->PopLock(name);
+#endif
+
     //fetch_sub는 변하기 이전값을 뱉어줌
     if ((_lockFlag.fetch_sub(1) & READ_COUNT_MASK) == 0)
         CRASH("MULTIPLE_UNLOCK");
